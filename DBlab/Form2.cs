54514cd63068c832accd125df63c56d9792fd8f0
DeividasVal <105603552+DeividasVal@users.noreply.github.com>
2023-06-12 21:45:35 +0300

    Add files via upload

---
+++ DBlab/Form2.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBlab
+{
+    public partial class Form2 : Form
+    {
+        string connetionString = "Data Source= localhost\\SQLEXPRESS; Database= ld3; Integrated Security=SSPI;";
+        string selectedPreke = "";
+
+        public Form2()
+        {
+            InitializeComponent();
+            using (SqlConnection sqlCon = new SqlConnection(connetionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT PREKES_PAV, PREKES_ID FROM PREKE", sqlCon);
+                DataSet ds = new DataSet();
+
+                sqlDa.Fill(ds);
+                comboBox2.DataSource = ds.Tables[0];
+                comboBox2.DisplayMember = "PREKES_PAV";
+                comboBox2.ValueMember = "PREKES_ID";
+                sqlCon.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon3 = new SqlConnection(connetionString))
+            {
+                sqlCon3.Open();
+                SqlCommand cmd1 = sqlCon3.CreateCommand();
+                cmd1.CommandText = "INSERT INTO TVARKO VALUES('" + comboBox1.SelectedValue+ "','" + comboBox2.SelectedValue + "')";
+                cmd1.ExecuteNonQuery();
+                sqlCon3.Close();
+                MessageBox.Show("Sėkmingai įdėta.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (selectedPreke != "")
+            {
+                    using (SqlConnection sqlCon = new SqlConnection(connetionString))
+                    {
+                        sqlCon.Open();
+                        SqlCommand cmd = sqlCon.CreateCommand();
+                        cmd.CommandText = "UPDATE PREKE SET PREKES_PAV= '" + comboBox2.GetItemText(this.comboBox2.SelectedItem) + "', PREKES_KAINA= '" + textBox3.Text + "', PREKIU_KIEKIS='" + textBox2.Text + "', APRASYMAS='" + textBox4.Text + "' WHERE PREKES_ID= '" + selectedPreke + "'";
+                        cmd.ExecuteNonQuery();
+                        sqlCon.Close();
+                        MessageBox.Show("Sėkmingai atnaujinta.");
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Pasirinkite prekę!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            comboBox2.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon2 = new SqlConnection(connetionString);
+
+            sqlCon2.Open();
+            SqlDataAdapter sqlDa2 = new SqlDataAdapter("select tvarko.PREKES_ID, TVARKO.DARBUOTOJO_ID, PREKE.PREKES_KAINA, PREKE.PREKES_PAV, PREKE.PREKIU_KIEKIS, PREKE.APRASYMAS FROM PREKE, DARBUOTOJAS, TVARKO WHERE PREKE.PREKES_ID = TVARKO.PREKES_ID AND TVARKO.DARBUOTOJO_ID = '"+comboBox1.SelectedValue+"' AND DARBUOTOJAS.DARBUOTOJO_ID = '"+comboBox1.SelectedValue+"'", sqlCon2);
+            DataTable dtbl2 = new DataTable();
+            sqlDa2.Fill(dtbl2);
+
+            dataGridView1.DataSource = dtbl2;
+            sqlCon2.Close();
+
+
+            dataGridView1.Columns["PREKES_ID"].Visible = false;
+            dataGridView1.Columns["DARBUOTOJO_ID"].Visible = false;
+
+            dataGridView1.Columns["PREKES_PAV"].DisplayIndex = 0;
+            dataGridView1.Columns["PREKES_KAINA"].DisplayIndex = 1;
+            dataGridView1.Columns["PREKIU_KIEKIS"].DisplayIndex =2;
+            dataGridView1.Columns["APRASYMAS"].DisplayIndex = 3;
+
+
+            dataGridView1.Columns[2].HeaderText = "Prekės kaina";
+            dataGridView1.Columns[3].HeaderText = "Prekės pavadinimas";
+            dataGridView1.Columns[4].HeaderText = "Kiekis";
+            dataGridView1.Columns[5].HeaderText = "Aprašymas";
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+
+                selectedPreke = row.Cells["PREKES_ID"].Value.ToString();
+
+                comboBox2.Text = "";
+                comboBox2.SelectedText = row.Cells["PREKES_PAV"].Value.ToString();
+                textBox2.Text = row.Cells["PREKIU_KIEKIS"].Value.ToString();
+                textBox3.Text = row.Cells["PREKES_KAINA"].Value.ToString();
+                textBox4.Text = row.Cells["APRASYMAS"].Value.ToString();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connetionString))
+            {
+                sqlCon.Open();
+                SqlCommand cmd = sqlCon.CreateCommand();
+                cmd.CommandText = "DELETE FROM TVARKO WHERE PREKES_ID = '" + selectedPreke + "'";
+                cmd.ExecuteNonQuery();
+                sqlCon.Close();
+                MessageBox.Show("Sėkmingai pašalinta.");
+            }
+        }
+    }
+}
---
+++ DBlab/Form9.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBlab
+{
+    public partial class Form9 : Form
+    {
+        string connetionString = "Data Source= localhost\\SQLEXPRESS; Database= ld3; Integrated Security=SSPI;";
+        string selectedNuolaida = "";
+        string selectedUzsakymas = "";
+
+        decimal kur;
+        public Form9()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rowCount = dataGridView1.Rows.Count;
+
+            if (rowCount <= 1)
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connetionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand cmd = sqlCon.CreateCommand();
+                    cmd.CommandText = "INSERT INTO NUOLAIDOS_KODAI (PROCENTAS, NUOLAIDOS_KOD)" +
+                    "VALUES ('" + textBox1.Text + "', '" + textBox2.Text + "')";
+                    cmd.ExecuteNonQuery();
+                    sqlCon.Close();
+                }
+
+                using (SqlConnection sqlCon = new SqlConnection(connetionString))
+                {
+                    sqlCon.Open();
+
+                    SqlCommand cmd1 = sqlCon.CreateCommand();
+                    cmd1.CommandText = "select NUOLAIDOS_ID FROM NUOLAIDOS_KODAI WHERE NUOLAIDOS_KODAI.NUOLAIDOS_KOD = '" + textBox2.Text + "'";
+                    SqlDataReader dr = cmd1.ExecuteReader();
+                    dr.Read();
+                    kur = dr.GetDecimal(0);
+                    sqlCon.Close();
+                }
+
+                using (SqlConnection sqlCon = new SqlConnection(connetionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand cmd = sqlCon.CreateCommand();
+                    cmd.CommandText = "UPDATE UZSAKYMAS SET NUOLAIDOS_ID= '" + kur + "' WHERE UZSAKYMO_ID = '" + comboBox1.SelectedValue + "'";
+                    cmd.ExecuteNonQuery();
+                    sqlCon.Close();
+                    MessageBox.Show("Sėkmingai įdėta.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Užsakymas gali turėti tik vieną nuolaidą!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connetionString))
+            {
+                sqlCon.Open();
+                SqlCommand cmd = sqlCon.CreateCommand();
+                cmd.CommandText = "DELETE FROM NUOLAIDOS_KODAI WHERE NUOLAIDOS_ID = '" + selectedNuolaida + "'";
+                cmd.ExecuteNonQuery();
+                sqlCon.Close();
+                MessageBox.Show("Sėkmingai pašalinta.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (selectedNuolaida != "")
+            {
+                if (textBox1.Text != "" && textBox2.Text != "")
+                {
+                            using (SqlConnection sqlCon = new SqlConnection(connetionString))
+                            {
+                                sqlCon.Open();
+                                SqlCommand cmd = sqlCon.CreateCommand();
+                                cmd.CommandText = "UPDATE NUOLAIDOS_KODAI SET PROCENTAS= '" + textBox1.Text + "', NUOLAIDOS_KOD = '"+textBox2.Text+"' WHERE NUOLAIDOS_ID = '" + selectedNuolaida + "'";
+                                cmd.ExecuteNonQuery();
+                                sqlCon.Close();
+                                MessageBox.Show("Sėkmingai atnaujinta.");
+                            }
+                }
+                else
+                {
+                    MessageBox.Show("Negali būti tušti laukai!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pasirinkite nuolaidą!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon2 = new SqlConnection(connetionString);
+
+            sqlCon2.Open();
+            SqlDataAdapter sqlDa2 = new SqlDataAdapter("SELECT UZSAKYMAS.UZSAKYMO_ID, NUOLAIDOS_KODAI.NUOLAIDOS_ID, NUOLAIDOS_KODAI.PROCENTAS, NUOLAIDOS_KODAI.NUOLAIDOS_KOD FROM UZSAKYMAS, NUOLAIDOS_KODAI WHERE UZSAKYMAS.UZSAKYMO_ID = '"+comboBox1.SelectedValue+"' AND UZSAKYMAS.NUOLAIDOS_ID = NUOLAIDOS_KODAI.NUOLAIDOS_ID", sqlCon2);
+            DataTable dtbl2 = new DataTable();
+            sqlDa2.Fill(dtbl2);
+
+            dataGridView1.DataSource = dtbl2;
+            sqlCon2.Close();
+
+            dataGridView1.Columns["UZSAKYMO_ID"].Visible = false;
+            dataGridView1.Columns["NUOLAIDOS_ID"].Visible = false;
+
+            dataGridView1.Columns[2].HeaderText = "Nuolaidos procentas";
+            dataGridView1.Columns[3].HeaderText = "Nuolaidos kodo pavadinimas";
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+
+                selectedNuolaida = row.Cells["NUOLAIDOS_ID"].Value.ToString();
+                selectedUzsakymas = row.Cells["UZSAKYMO_ID"].Value.ToString();
+
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox1.SelectedText = row.Cells["PROCENTAS"].Value.ToString();
+                textBox2.Text = row.Cells["NUOLAIDOS_KOD"].Value.ToString();
+            }
+        }
+    }
+}
